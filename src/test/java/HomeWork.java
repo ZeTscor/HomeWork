a4e53db2dea2a8910162dc4fdc7d0c3ee5d1335f
artem <poi.ru0130@gmail.com>
2021-10-28 00:26:17 +0300

    remove Configuration.holdOpen and add comments

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -16,35 +16,54 @@ public class HomeWork {
      void Option(){
         Configuration.startMaximized = true;
         open("https://demoqa.com/automation-practice-form");
-        Configuration.holdBrowserOpen = true;
+
 
     }
     static String PHONE_NUMBER = "7888999777";
     @Test
     public void StudentRegForm(){
 
+        //personal information
         $("#firstName").setValue("Artem");
         $("#lastName").setValue("Kulakov");
         $("#userEmail").setValue("test@gmail.com");
         $(byText("Male")).click();
         $("#dateOfBirthInput").click();
         $("#userNumber").setValue(PHONE_NUMBER);
+
+        //date birthday
         $(byClassName("react-datepicker__year-select")).click();
         $(byClassName("react-datepicker__year-select")).selectOptionByValue("2000");
         $(byClassName("react-datepicker__month-select")).click();
         $(byClassName("react-datepicker__month-select")).selectOptionContainingText("April");
         $(byText("4")).click();
+
+        //subjects field
         $x("//*[@id=\"subjectsContainer\"]/div").click();
         $("#subjectsInput").setValue("Com").pressEnter();
+
+        //Hobbies check box
         $(byText("Reading")).click();
         $(byText("Music")).click();
+
+        //upload picture
         $("#uploadPicture").uploadFile(new File("uploadFiles/1.png"));
+
+        //address
         $("#currentAddress").setValue("Russia, Moscow");
+
+        //input state title
         $("#state").scrollIntoView(true).click();
         $("#react-select-3-input").setValue("harya").pressEnter();
+
+        //input city title
         $("#city").click();
         $("#react-select-4-input").setValue("Karnal").pressEnter();
+
+        //click submit button
         $("#submit").click();
+
+        //check for form opening
         $(byText("Thanks for submitting the form")).shouldHave(Condition.visible);
 
 
