24af36b4ba202b6da203db02bcdbe052146bca0f
artem <poi.ru0130@gmail.com>
2021-10-28 16:28:03 +0300

    changes according to comments

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,8 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.Keys;
-
 
 import java.io.File;
 
@@ -10,28 +8,38 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
-
 public class HomeWork {
-    @BeforeEach
-     void Option(){
-        Configuration.startMaximized = true;
-        open("https://demoqa.com/automation-practice-form");
 
-
-    }
+    static String FIRST_NAME = "Artem";
+    static String LAST_NAME = "Kulakov";
+    static String USER_EMAIL = "test@gmail.com";
     static String PHONE_NUMBER = "7888999777";
+    static String ADDRESS = "Russia, Moscow";
+    static String STATE = "harya";
+    static String CITY = "Karnal";
+
+    @BeforeAll
+    static void ConfigAllTest() {
+        Configuration.startMaximized = true;
+    }
+
+    @BeforeEach
+    void Option() {
+        open("https://demoqa.com/automation-practice-form");
+    }
+
     @Test
-    public void StudentRegForm(){
+    public void StudentRegForm() {
 
         //personal information
-        $("#firstName").setValue("Artem");
-        $("#lastName").setValue("Kulakov");
-        $("#userEmail").setValue("test@gmail.com");
+        $("#firstName").setValue(FIRST_NAME);
+        $("#lastName").setValue(LAST_NAME);
+        $("#userEmail").setValue(USER_EMAIL);
         $(byText("Male")).click();
-        $("#dateOfBirthInput").click();
         $("#userNumber").setValue(PHONE_NUMBER);
 
         //date birthday
+        $("#dateOfBirthInput").click();
         $(byClassName("react-datepicker__year-select")).click();
         $(byClassName("react-datepicker__year-select")).selectOptionByValue("2000");
         $(byClassName("react-datepicker__month-select")).click();
@@ -50,23 +58,21 @@ public class HomeWork {
         $("#uploadPicture").uploadFile(new File("uploadFiles/1.png"));
 
         //address
-        $("#currentAddress").setValue("Russia, Moscow");
+        $("#currentAddress").setValue(ADDRESS);
 
         //input state title
         $("#state").scrollIntoView(true).click();
-        $("#react-select-3-input").setValue("harya").pressEnter();
+        $("#react-select-3-input").setValue(STATE).pressEnter();
 
         //input city title
         $("#city").click();
-        $("#react-select-4-input").setValue("Karnal").pressEnter();
+        $("#react-select-4-input").setValue(CITY).pressEnter();
 
         //click submit button
         $("#submit").click();
 
         //check for form opening
         $(byText("Thanks for submitting the form")).shouldHave(Condition.visible);
-
-
 
 
     }
